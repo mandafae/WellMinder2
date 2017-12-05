3840036662fa70e66ebfad62a25138c08cfaa650
“mandafae“ <mandafae5@gmail.com>
2017-12-05 15:19:54 -0600

    start google auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
 import Navbar from './components/navbar'
 import Dashboard from './components/dashboard'
 import Landing from './components/landing'
@@ -7,14 +8,24 @@ import Quiz from './components/quiz'
 import './App.css';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyBc5KaDNcBS3wWN1kTVtWTGgi2MVjGgZko",
+      authDomain: "wellminder-7beb4.firebaseapp.com",
+      databaseURL: "https://wellminder-7beb4.firebaseio.com",
+      projectId: "wellminder-7beb4",
+      storageBucket: "wellminder-7beb4.appspot.com",
+      messagingSenderId: "707860039026"
+    };
+    firebase.initializeApp(config);
+    const database = firebase.database();
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
-        <Dashboard />
         <Landing />
-        <Preferences />
-        <Quiz />
       </div>
     );
   }
