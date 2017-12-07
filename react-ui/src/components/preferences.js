34540eb15cc5b3653a21030a9c090dd85ed9d612
“mandafae“ <mandafae5@gmail.com>
2017-12-07 13:06:11 -0600

    preferences get updated in parent and child component, checkboxes display correctly

--- react-ui/src/components/preferences.js
+++ react-ui/src/components/preferences.js
@@ -11,35 +11,35 @@ class Preferences extends Component {
 
   onChange = (event) => {
     if(event.target.name === 'sleep'){
-      this.setState({ sleep: !event.target.value }, () => {
+      this.setState({ sleep: !this.state.sleep }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'diet'){
-      this.setState({ diet: !event.target.value }, () => {
+      this.setState({ diet: !this.state.diet }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'activity'){
-      this.setState({ activity: !event.target.value }, () => {
+      this.setState({ activity: !this.state.activity }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'emotional'){
-      this.setState({ emotional: !event.target.value }, () => {
+      this.setState({ emotional: !this.state.emotional }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'social'){
-      this.setState({ social: !event.target.value }, () => {
+      this.setState({ social: !this.state.social }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'occupational'){
-      this.setState({ occupational: !event.target.value }, () => {
+      this.setState({ occupational: !this.state.occupational }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'spiritual'){
-      this.setState({ spiritual: !event.target.value }, () => {
+      this.setState({ spiritual: !this.state.spiritual }, () => {
         //console.log(this.state)
       });
     }else if(event.target.name === 'intellectual'){
-      this.setState({ intellectual: !event.target.value }, () => {
+      this.setState({ intellectual: !this.state.intellectual }, () => {
         //console.log(this.state)
       });
     }
@@ -53,14 +53,14 @@ class Preferences extends Component {
         <h3 className = "cardTitle">User Preferences</h3>
         <hr className="divider"></hr>
         <h5>Which wellness aspects would you like to track?</h5>
-        <div className="prefList"><label>Sleep</label><input type="checkbox" name="sleep" checked={this.state.user.userData.preferences.sleep} onClick={this.onChange} /></div>
-        <div className="prefList"><label>Diet</label><input type="checkbox" name="diet" onClick={this.onChange} defaultChecked /></div>
-        <div className="prefList"><label>Activity</label><input type="checkbox" name="activity" onClick={this.onChange} defaultChecked /></div>
-        <div className="prefList"><label>Emotional</label><input type="checkbox" name="emotional" onClick={this.onChange} defaultChecked /></div>
-        <div className="prefList"><label>Social</label><input type="checkbox" name="social" onClick={this.onChange} defaultChecked /></div>
-        <div className="prefList"><label>Occupational</label><input type="checkbox" name="occupational" onClick={this.onChange} defaultChecked /></div>
-        <div className="prefList"><label>Spiritual</label><input type="checkbox" name="spiritual" onClick={this.onChange} defaultChecked /></div>
-        <div className="prefList"><label>Intellectual</label><input type="checkbox" name="intellectual" onClick={this.onChange} defaultChecked /></div>
+        <div className="prefList"><label>Sleep</label><input type="checkbox" name="sleep" defaultChecked={this.state.sleep} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Diet</label><input type="checkbox" name="diet" defaultChecked={this.state.diet} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Activity</label><input type="checkbox" name="activity" defaultChecked={this.state.activity} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Emotional</label><input type="checkbox" name="emotional" defaultChecked={this.state.emotional} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Social</label><input type="checkbox" name="social" defaultChecked={this.state.social} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Occupational</label><input type="checkbox" name="occupational" defaultChecked={this.state.occupational} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Spiritual</label><input type="checkbox" name="spiritual" defaultChecked={this.state.spiritual} onClick={this.onChange} /></div>
+        <div className="prefList"><label>Intellectual</label><input type="checkbox" name="intellectual" defaultChecked={this.state.intellectual} onClick={this.onChange} /></div>
         <Link to="/dashboard"><button className="formSubmit" onClick={() => { this.props.handlePreferences(this.state) }}>Submit</button></Link>
       </form>
     </div>
