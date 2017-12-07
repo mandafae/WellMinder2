2d68fb832f037067d87b2673dc58ee4c44af2e92
“mandafae“ <mandafae5@gmail.com>
2017-12-07 15:44:31 -0600

    quiz questions display based on user preferences

--- react-ui/src/components/quiz.js
+++ react-ui/src/components/quiz.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Quiz extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { date: new Date(), sleep: null, diet: null, activity: null, emotional: null, social: null, occupational: null, spiritual: null, intellectual: null }
+    console.log("Quiz props:", props);
+    this.state = { date: new Date(), sleep: 0, diet: 0, activity: 0, emotional: 0, social: 0, occupational: 0, spiritual: 0, intellectual: 0 }
     this.handleClick = this.handleClick.bind(this);
     this.formHandler = this.formHandler.bind(this);
   }
@@ -50,80 +52,108 @@ class Quiz extends Component {
   }
 
   render() {
+    let sleepPref = this.props.user.userData.preferences.sleep;
+    let dietPref = this.props.user.userData.preferences.diet;
+    let activityPref = this.props.user.userData.preferences.activity;
+    let emotionalPref = this.props.user.userData.preferences.emotional;
+    let socialPref = this.props.user.userData.preferences.social;
+    let occupationalPref = this.props.user.userData.preferences.occupational;
+    let spiritualPref = this.props.user.userData.preferences.spiritual;
+    let intellectualPref = this.props.user.userData.preferences.intellectual;
+
     return (
     <div className = "card quiz">
       <h3 className="cardTitle">Daily Check In</h3>
       <hr className="divider"></hr>
       <form className = "quizForm">
+
         {/* SLEEP */}
-        <label className="quizLabel">Sleep</label>
+        { sleepPref ?
+        <frag><label className="quizLabel">Sleep</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="sleep" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="sleep" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="sleep" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="sleep" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="sleep" value="5" />Very Good
-        <textarea className='notes' name="sleep" placeholder='Notes' />
+        <textarea className='notes' name="sleep" placeholder='Notes' /></frag>
+        : null }
+
         { /*DIET */}
-        <label className="quizLabel">Diet</label>
+        { dietPref ?
+        <frag><label className="quizLabel">Diet</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="diet" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="diet" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="diet" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="diet" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="diet" value="5" />Very Good
-        <textarea className='notes' name="diet" placeholder='Notes' />
+        <textarea className='notes' name="diet" placeholder='Notes' /></frag>
+        : null }
+
         {/* ACTIVITY */}
-        <label className="quizLabel">Activity</label>
+        { activityPref ?
+        <frag><label className="quizLabel">Activity</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="activity" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="activity" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="activity" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="activity" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="activity" value="5" />Very Good
-        <textarea className='notes' name="activity" placeholder='Notes' />
+        <textarea className='notes' name="activity" placeholder='Notes' /></frag>
+        : null }
 
         {/* EMOTIONAL */}
-        <label className="quizLabel">Emotional</label>
+        { emotionalPref ?
+        <frag><label className="quizLabel">Emotional</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="emotional" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="emotional" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="emotional" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="emotional" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="emotional" value="5" />Very Good
-        <textarea className='notes' name="emotional" placeholder='Notes' />
+        <textarea className='notes' name="emotional" placeholder='Notes' /></frag>
+        : null }
 
         {/* SOCIAL */}
-        <label className="quizLabel">Social</label>
+        { socialPref ?
+        <frag><label className="quizLabel">Social</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="social" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="social" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="social" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="social" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="social" value="5" />Very Good
-        <textarea className='notes' name="social" placeholder='Notes' />
+        <textarea className='notes' name="social" placeholder='Notes' /></frag>
+        : null }
 
         {/* OCCUPATIONAL */}
-        <label className="quizLabel">Occupational</label>
+        { occupationalPref ?
+        <frag><label className="quizLabel">Occupational</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="occupational" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="occupational" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="occupational" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="occupational" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="occupational" value="5" />Very Good
-        <textarea className='notes' name="occupational" placeholder='Notes' />
+        <textarea className='notes' name="occupational" placeholder='Notes' /></frag>
+        : null }
 
         {/* SPIRITUAL */}
-        <label className="quizLabel">Spiritual</label>
+        { spiritualPref ?
+        <frag><label className="quizLabel">Spiritual</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="spiritual" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="spiritual" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="spiritual" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="spiritual" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="spiritual" value="5" />Very Good
-        <textarea className='notes' name="spiritual" placeholder='Notes' />
+        <textarea className='notes' name="spiritual" placeholder='Notes' /></frag>
+        : null }
 
         {/* INTELLECTUAL */}
-        <label className="quizLabel">Intellectual</label>
+        { intellectualPref ?
+        <frag><label className="quizLabel">Intellectual</label>
         <input onClick={this.handleClick} className='likert' type="radio" name="intellectual" value="1" />Very Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="intellectual" value="2" />Poor
         <input onClick={this.handleClick} className='likert' type="radio" name="intellectual" value="3" />Neutral
         <input onClick={this.handleClick} className='likert' type="radio" name="intellectual" value="4" />Good
         <input onClick={this.handleClick} className='likert' type="radio" name="intellectual" value="5" />Very Good
-        <textarea className='notes' name="intellectual" placeholder='Notes' />
+        <textarea className='notes' name="intellectual" placeholder='Notes' /></frag>
+        : null }
 
         <button className="formSubmit" onClick={this.formHandler}>Submit</button>
       </form>
