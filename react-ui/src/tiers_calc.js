a5413dbf63634d2b2cd97a7fe5d9e6e0d2d2872d
Rachel Cavin <Rmcavin@gmail.com>
2017-12-08 10:15:01 -0600

    remove negative scores

--- react-ui/src/tiers_calc.js
+++ react-ui/src/tiers_calc.js
@@ -1,12 +1,16 @@
 let aspects = ["sleep", "diet", "activity", "emotional", "social", "occupational", "spiritual", "intellectual"]
 export function scoreCalc(quizResults, currentScore) {
+  console.log('this is the data that comes into scoreCalc', quizResults, CurrentScore)
   let dailyScores = {};
-  let scoreMap = {1:-2, 2:-1, 3:0, 4:1, 5:2}
+  let scoreMap = {0:0,1:-2, 2:-1, 3:0, 4:1, 5:2}
   //aspect scores
   aspects.forEach((aspect) => {
     if (quizResults[aspect]) {
       let points = scoreMap[quizResults[aspect]]
       let score = currentScore[aspect] + points;
+      if (score < 0) {
+        score = 0;
+      }
       dailyScores[aspect] = score;
     } else {
       dailyScores[aspect] = 0;
@@ -16,6 +20,9 @@ export function scoreCalc(quizResults, currentScore) {
   let totalScore = Object.values(dailyScores).reduce( (acc, val) => {
     return acc + val
   },0)
+  if (totalScore < 0) {
+    totalScore = 0;
+  }
   dailyScores.total = totalScore;
   //streak score
   let today = Date.parse(quizResults.date);
@@ -29,10 +36,15 @@ export function scoreCalc(quizResults, currentScore) {
   else if (days >= 1) {
     dailyScores.streak = currentScore.streak++
   }
+  if (dailyScores.streak < 0) {
+    dailyScores.streak = 0;
+  }
   return dailyScores;
 }
 
 export function tierCalc(scores) {
+
+  console.log('this is the data that comes into tierCalc',scores)
   let totalTiers = {0:"level one",28:"level two", 42:"level three", 63:"level four", 95:"level five", 142:"level six", 214:"level seven", 320:"level eight", 480:"level nine", 720:"level ten"};
   let aspectTiers = {0:"level one",10:"level two", 20:"level three", 30:"level four", 40:"level five", 50:"level six", 60:"level seven", 70:"level eight", 80:"level nine", 90:"level ten"};
   let streakTiers = {0:"level one",1:"level two", 2:"level three", 3:"level four", 4:"level five", 5:"level six", 6:"level seven", 7:"level eight", 8:"level nine", 9:"level ten"};
